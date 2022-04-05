bd8fa70fa5f96bad3e476e89a3e80ab2b0c95f32
SimoAl17 <simo.al.17w@gmail.com>
2022-04-05 13:22:34 +0200

    initial commit

---
+++ script.js
@@ -0,0 +1,74 @@
+let counter = 0;
+
+window.addEventListener("state-update", (e) => updateCounter(e.detail));
+
+const manager = new StateManager(counter);
+
+function plusOne() {
+  dispatchEvent(new CustomEvent("plus-one"));
+}
+
+function minusOne() {
+  dispatchEvent(new CustomEvent("minus-one"));
+}
+
+function undo() {
+    dispatchEvent(new CustomEvent("undo-action"));
+}
+
+function redo() {
+    dispatchEvent(new CustomEvent("redo-action"));
+}
+
+function updateCounter(state) {
+  const counterLabel = document.getElementById("counter");
+  counterLabel.innerHTML = state;
+}
+
+function reset() {
+    manager.reset();
+    updateCounter();
+}
+
+// let counterHistory = [0];
+
+// let historyIndex = 0;
+
+// function plusOne() {
+//     counter ++;
+//     counterHistory = counterHistory.slice(0, historyIndex + 1);
+//     counterHistory.push(counter);
+//     historyIndex ++;
+//     console.log(counterHistory);
+//     updateCounter();
+// }
+
+// function minusOne() {
+//     counter --;
+//     counterHistory = counterHistory.slice(0, historyIndex + 1);
+//     counterHistory.push(counter);
+//     historyIndex ++;
+//     console.log(counterHistory);
+//     updateCounter();
+// }
+
+// function updateCounter() {
+//     const counterLabel = document.getElementById('counter');
+//     counterLabel.innerHTML = counter;
+// }
+
+// function undo() {
+//     if (historyIndex > 0) {
+//         historyIndex --;
+//         counter = counterHistory[historyIndex];
+//         updateCounter();
+//     }
+// }
+
+// function redo() {
+//     if (historyIndex < counterHistory.length-1) {
+//         historyIndex ++;
+//         counter = counterHistory[historyIndex];
+//         updateCounter();
+//     }
+// }
---
+++ state-manager.js
@@ -0,0 +1,85 @@
+class StateManager {
+
+    actions = {
+        "ADD_ONE": (state) => {
+           return state + 1;
+        },
+        "REMOVE_ONE": (state) => {
+           return state - 1;
+        }
+    }
+
+    constructor(startingState) {
+    this.startingState = startingState;
+    this.state = startingState;
+
+    this.actionsList = [];
+    this.actionsIndex = 0;
+
+    window.addEventListener('plus-one', (e) => this.addOne());
+    window.addEventListener('minus-one', (e) => this.removeOne());
+    window.addEventListener('undo-action', (e) => this.undo());
+    window.addEventListener('redo-action', (e) => this.redo());
+    }
+
+    addOne() {
+        this.resetFuture();
+        this.actionsList.push("ADD_ONE");
+        this.actionsIndex ++;
+        this.limitActionsList();
+        console.log(this.actionsList);
+        this.reducer();
+    }
+
+    removeOne() {
+        this.resetFuture();
+        this.actionsList.push("REMOVE_ONE");
+        this.actionsIndex ++;
+        this.limitActionsList();
+        console.log(this.actionsList);
+        this.reducer();
+    }
+
+    undo() {
+        if (this.actionsIndex > 0) {
+            this.actionsIndex --;
+            this.reducer();    
+        }
+    }
+
+    redo() {
+        if (this.actionsIndex < this.actionsList.length) {
+            this.actionsIndex ++;
+            this.reducer();
+        }
+    }
+
+    reducer() {
+        this.state = this.actionsList.reduce((state, action, index) => {
+            return index < this.actionsIndex ? this.actions[action](state) : state;
+        }, this.startingState);
+        console.log(this.state);
+        dispatchEvent(new CustomEvent("state-update", {detail: this.state}));
+    }
+
+    resetFuture() {
+        this.actionsList = this.actionsList.slice(0, this.actionsIndex);
+    }
+
+    reset() {
+        this.actionsList = [];
+        this.actionsIndex = 0;
+        this.startingState = this.state;
+    }
+
+    limitActionsList() {
+        if (this.actionsList.length > 3) {
+            this.lostAction = this.actionsList.slice(0, 1);
+            this.actionsList = this.actionsList.slice(1, this.actionsList.length);
+            this.actionsIndex --;
+            const actionName = this.lostAction[0];
+            this.startingState = this.actions[actionName](this.startingState);
+        }
+    }
+   
+}
